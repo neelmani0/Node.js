1dc35dfe144c14fecd8d4d73d96a5a4648ff47d8
neelmani0 <neelmanibcet@gmail.com>
2024-09-25 20:20:38 +0530

    2nd commit

--- S3/Day1/index.js
+++ S3/Day1/index.js
@@ -4,7 +4,23 @@ const main = async ()=>{
     try {
         const connection = await mongoose.connect("mongodb://127.0.0.1:27017/webnxm");
         console.log("Connection Established");
-       await UserModel.insertMany([{name:"Grue",age:21,city:"AP"},{name:"Viru",age:22,city:"Hyd"}])
+ 
+        // insertion of one doucumet 
+    //    await UserModel.insertMany([{name:"Grue",age:21,city:"AP"},{name:"Viru",age:22,city:"Hyd"}])
+
+         // insertion of one doucumet 
+    //   const user = new UserModel({
+    //     name:"Thor",
+    //     age:"300",
+    //     city:"assgard"
+    //   })
+    //   user.save()
+
+    console.log("Following is the data present in database")
+    //  const user = await UserModel.find()
+    // const user = await UserModel.find({$and:[{age:{$gte:20}},{age:{$lte:40}}]})
+    //console.log(user)
+     await UserModel.deleteMany({name:"Thor"})
         connection.didconnect() 
     } catch (error) {
         console.log(error.message)
